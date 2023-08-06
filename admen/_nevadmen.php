80a7c23f2736c2c1a79b5c11303cce8cc184e681
abubakar <abubakarjuttabubakar553@gmail.com>
2023-08-06 00:39:19 -0700

    solving error

--- admen/_nevadmen.php
+++ admen/_nevadmen.php
@@ -11,7 +11,7 @@ else
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FOOD WEB</title>
+	
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>FOOD WEB</title>
